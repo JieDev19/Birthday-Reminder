12d989de4e7926e70eb35c5718b24a34b083f7c4
Jie Burillo <benjieburillo21@gmail.com>
2022-12-25 23:53:48 +0800

    display birthday list

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from "react"
+import "./index.css"
+import { List } from "./List.jsx"
 
-function App() {
-  const [count, setCount] = useState(0)
+const Data = [
+    {
+        id: 1,
+        name: "Bertie Yates",
+        age: 29,
+        image:
+            "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
+    },
+    {
+        id: 2,
+        name: "Hester Hogan",
+        age: 32,
+        image:
+            "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg",
+    },
+    {
+        id: 3,
+        name: "Larry Little",
+        age: 36,
+        image:
+            "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    },
+    {
+        id: 4,
+        name: "Sean Walsh",
+        age: 34,
+        image:
+            "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    },
+    {
+        id: 5,
+        name: "Lola Gardner",
+        age: 29,
+        image:
+            "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    },
+]
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+function App(props) {
+    const [birthday, setBirthday] = useState(Data)
+
+    function btnClear() {
+        setBirthday([])
+    }
+
+    return (
+        <>
+            <main>
+                <section className={"container"}>
+                    <h3>{birthday.length} birthday today</h3>
+                    <List people={birthday}/>
+                    <button onClick={btnClear}>clear all</button>
+                </section>
+            </main>
+        </>
+    )
 }
 
 export default App
---
+++ src/List.jsx
@@ -0,0 +1,19 @@
+export function List({people}) {
+    return (
+        <>
+            {people.map((person) => {
+                const {id, name, age, image} = person
+
+                return (
+                    <article key={id} className={"person"}>
+                        <img src={image} alt="person"/>
+                        <div>
+                            <h4>{name}</h4>
+                            <p>{age} years</p>
+                        </div>
+                    </article>
+                )
+            })}
+        </>
+    )
+}
